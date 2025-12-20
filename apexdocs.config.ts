a7332f427e2e54de638c289df26fcfb41645bb5f
Russell Laboe <russell-laboe@outlook.com>
2025-12-19 23:34:41 -0700

    docs: implemented autodocs

---
+++ apexdocs.config.ts
@@ -0,0 +1,113 @@
+import { defineChangelogConfig, defineMarkdownConfig, DocPageData } from '@cparra/apexdocs';
+import * as fs from 'node:fs';
+
+function loadFileAsync(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function writeFileAsync(filePath: string, data: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, data, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export default {
+  changelog: defineChangelogConfig({
+    previousVersionDir: 'previous',
+    currentVersionDir: 'force-app/src',
+    scope: ['global', 'public', 'protected', 'private', 'namespaceaccessible'],
+    transformChangeLogPage: () => {
+      return {
+        frontmatter: {
+          title: 'Changelog',
+        },
+      };
+    },
+  }),
+  markdown: defineMarkdownConfig({
+    sourceDir: 'force-app/src',
+    includeMetadata: false,
+    scope: ['global', 'public', 'protected', 'private', 'namespaceaccessible'],
+    sortAlphabetically: true,
+    namespace: 'apexdocs',
+    transformReference: (reference) => {
+      return {
+        // remove the trailing .md
+        referencePath: reference.referencePath.replace(/\.md$/, ''),
+      };
+    },
+    transformReferenceGuide: async () => {
+      const frontMatter = await loadFileAsync('./docs/index-frontmatter.md');
+      return {
+        frontmatter: frontMatter,
+      };
+    },
+    excludeTags: ['internal'],
+    transformDocs: async (docs) => {
+      const apexOnlyDocs = docs.filter((doc) => doc.type !== 'customobject');
+      const objectOnlyDocs = docs.filter((doc) => doc.type === 'customobject');
+
+      // Update sidebar
+      const sidebar = [
+        {
+          text: 'API Reference',
+          items: Array.from(extractGroups(apexOnlyDocs)).map(([groupName, groupDocs]) => ({
+            text: groupName,
+            items: groupDocs.map(toSidebarLink),
+          })),
+        },
+        {
+          text: 'Object Reference',
+          items: objectOnlyDocs.map(toSidebarLink),
+        },
+      ];
+      await writeFileAsync('./docs/.vitepress/sidebar.json', JSON.stringify(sidebar, null, 2));
+
+      return docs;
+    },
+    transformDocPage: async (docPage) => {
+      return {
+        ...docPage,
+        frontmatter: {
+          title: docPage.source.name,
+        },
+      };
+    },
+  }),
+};
+
+function toSidebarLink(doc: DocPageData) {
+  return {
+    text: doc.source.name,
+    link: doc.outputDocPath,
+  };
+}
+
+function extractGroups(docs: DocPageData[]) {
+  const groups = new Map<string, DocPageData[]>();
+  for (const doc of docs) {
+    if (!doc.group) {
+      continue;
+    }
+
+    const groupDocs = groups.get(doc.group) ?? [];
+    groupDocs.push(doc);
+    groups.set(doc.group, groupDocs);
+  }
+
+  return groups
+}
---
+++ docs/index-frontmatter.md
@@ -0,0 +1,16 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Apex Trigger Weaver"
+  text: "Take control of your triggers"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+---
